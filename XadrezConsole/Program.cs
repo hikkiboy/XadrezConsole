4b1472880601314a927d6b07c8d7b004b3507999
hikki <gustavoht2014@gmail.com>
2024-02-16 11:56:46 -0300

    excecoes de tabuleiro sendo tratadas

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -8,13 +8,21 @@ namespace XadrezConsole
     {
         static void Main(string[] args)
         {
-            Tabuleiros tab = new Tabuleiros(8, 8);
+            try
+            {
+                Tabuleiros tab = new Tabuleiros(8, 8);
 
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 4));
-            
-            Tela.imprimirTabuleiro(tab);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 4));
+
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e )
+            {
+                Console.WriteLine(e.Message);
+            }
+           
         }
     }
 }
