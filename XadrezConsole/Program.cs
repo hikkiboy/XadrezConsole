5b36727429bf2608218f92edff90e994c8e68cfa
hikki <gustavoht2014@gmail.com>
2024-02-16 11:13:07 -0300

    Classe Posicao done

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace XadrezConsole
+﻿using Tabuleiro;
+
+namespace XadrezConsole
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
