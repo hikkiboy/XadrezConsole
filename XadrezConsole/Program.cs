63806166c39c5267c2c836970bfc00e0a8ccaecc
hikki <gustavoht2014@gmail.com>
2024-02-17 00:43:11 -0300

    melhor impressao do tabuleiro

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -13,7 +13,6 @@ namespace XadrezConsole
                 Tabuleiros tab = new Tabuleiros(8, 8);
 
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
                 tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 4));
 
                 Tela.imprimirTabuleiro(tab);
--- XadrezConsole/Tela.cs
+++ XadrezConsole/Tela.cs
@@ -14,6 +14,8 @@ namespace XadrezConsole
         {
             for (int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
+
                 for (int j = 0; j < tab.Colunas; j++)
                 {
                     if(tab.peca(i,j) == null)
@@ -22,13 +24,30 @@ namespace XadrezConsole
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        ImprimirPeca(tab.peca(i, j));
+                        Console.Write(' ');
                     }
                     
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");
             
+        }
+
+        public static void ImprimirPeca (Peca peca)
+        {
+           if(peca.Cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
         }
     }
 }
