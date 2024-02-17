91a109fbcc2d61399f3b66da6458d4e1e32533de
hikki <gustavoht2014@gmail.com>
2024-02-17 00:28:32 -0300

    posicao xadrez feita

---
+++ XadrezConsole/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,27 @@
+﻿
+using Tabuleiro;
+
+namespace XadrezConsole.Xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char Coluna {  get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public override string ToString()
+        {
+            return "" + Linha + Coluna;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+    }
+}
